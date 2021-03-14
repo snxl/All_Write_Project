acdb76806588ada0ab60f476535bfd6b3908046e
Nicolas <nicolas.soares.gomes@gmail.com>
2021-03-14 19:17:30 -0300

    melhorias html e dropdown menu

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, './src/public')));
+app.use(express.static(path.join(__dirname, './src/assets/img/ *')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/index.html'))
+    res.sendFile(path.join(__dirname, './src/views/index.html'))
 })
 
 app.listen(80, () => {
