d00039a4a6adcb4aeb5fbe78b8122da3b549d189
Nicolas <nicolas.soares.gomes@gmail.com>
2021-03-21 19:11:00 -0300

    adicionado o tailwindcss

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const path = require('path')
+const exhbs = require('express-handlebars')
 const app = express()
+const path = require("path")
 
-app.use(express.static(path.join(__dirname, './src/assets/img/ *')));
+
+app.use(express.static(path.join(__dirname, './src/img')));
+app.use(express.static(path.join(__dirname, './src/styles')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './src/views/index.html'))
+    res.sendFile(path.join(__dirname, 'src/views/index.html'))
 })
 
-app.listen(80, () => {
-    console.log("Server is running")
+app.listen(80, ()=>{
+    console.log('server running')
 })
