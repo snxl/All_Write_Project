c2a3ef675e78496539a96fc16d3ee5bb87484e12
Nicolas <nicolas.soares.gomes@gmail.com>
2021-05-20 23:05:23 -0300

    alteração no sequelize

--- AllWriteMVC/config/mySQLConnection.js
+++ AllWriteMVC/config/mySQLConnection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Shine130102",
+    password: null,
     database: "register-allwrite"
 })
 
