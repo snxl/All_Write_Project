ed54e53c27e571f44ddf941de1ff3237f5e34c82
Ariel Pellegrino <ariel.pell@gmail.com>
2021-06-26 15:13:08 -0300

    update novos arquivos

--- AllWriteMVC/public/javascript/carousel.js
+++ AllWriteMVC/public/javascript/carousel.js
@@ -1,34 +1,9 @@
-const delay = 3000; //ms
+$(".my__books").slick();
 
-const slides = document.querySelector(".slides");
-const slidesCount = slides.childElementCount;
-const maxLeft = (slidesCount - 1) * 100 * -1;
-
-let current = 0;
-
-function changeSlide(next = true) {
-  if (next) {
-    current += current > maxLeft ? -100 : current * -1;
-  } else {
-    current = current < 0 ? current + 100 : maxLeft;
-  }
-
-  slides.style.left = current + "%";
-}
-
-let autoChange = setInterval(changeSlide, delay);
-const restart = function() {
-  clearInterval(autoChange);
-  autoChange = setInterval(changeSlide, delay);
-};
-
-// Controls
-document.querySelector(".next-slide").addEventListener("click", function() {
-  changeSlide();
-  restart();
-});
-
-document.querySelector(".prev-slide").addEventListener("click", function() {
-  changeSlide(false);
-  restart();
-});
+$('.my__books').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+    });
