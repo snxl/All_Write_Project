c12774a3f4219c0e88273083e5a082176bd78327
marcelogamon <79877585+marcelogamon@users.noreply.github.com>
2021-07-30 01:03:55 -0300

    subindo popUps na dashboard

---
+++ AllWriteMVC/public/javascripts/popUp.js
@@ -0,0 +1,9 @@
+function showModal() {
+    var element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function closeModal() {
+    var element = document.getElementById("modal");
+    element.classList.remove("show-modal");
+}
