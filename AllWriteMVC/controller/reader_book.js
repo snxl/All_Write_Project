14fde0e8f070a4bc049a5bd367fc1b8ce1200d7d
Nicolas <nicolas.soares.gomes@gmail.com>
2021-07-31 22:35:54 -0300

    renderização dinamica

--- AllWriteMVC/controller/reader_book.js
+++ AllWriteMVC/controller/reader_book.js
@@ -4,7 +4,11 @@ import jwt from "jsonwebtoken"
 class Reader_book{
     async GET(req, res){
 
+      try {
+
         const token = jwt.verify(req.cookies.token, process.env.TOKEN_SECRET)
+
+        const {id} = req.params
 
         const {route, user} = await db.Registro.findOne({
           where:{
@@ -12,11 +16,28 @@ class Reader_book{
           }
         })
 
+        const data = await db.Livros.findOne({
+          where:{
+            id
+          },
+          include:{
+            model: db.Registro,
+            as:"autor"
+          }
+        })
+
+        console.log(data)
+
         res.render("reader_book", {
             errorUser: false,
             profile: route,
-            user
+            user,
+            data
         })
+
+      } catch (error) {
+
+      }
     }
 
     POST(req, res){
